bd4efe9cd3f5aafb16afd0f803ac24022dcdad9b
Cadowyn <JamesChanceWilson@gmail.com>
2017-03-13 12:08:14 -0400

    Add files via upload

---
+++ Preparations Exercises 2.rb
@@ -0,0 +1,3 @@
+# Doing this as a comment to be careful about the rm risk!
+
+# rm -R my_folder
---
+++ Prepartations Excercises 1.rb
@@ -0,0 +1,11 @@
+# It is kind hard for me to save the command line as a specific file I believe,
+# so I just put in Sublime Text as to what I would do. 
+
+# mkdir my_folder
+# cd my_folder
+# touch one.rb
+# touch two.rb 
+# ruby one.rb 
+# ruby two.rb 
+
+
